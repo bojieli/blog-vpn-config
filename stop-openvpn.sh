50211c9ff68492d7d88c1bb557ad039705a7ae60
Bojie Li <bojieli@gmail.com>
2013-12-07 22:07:10 +0800

    add stop-openvpn.sh

---
+++ stop-openvpn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DAEMON=/usr/sbin/openvpn
+CONFDIR=/etc/openvpn
+
+[ `whoami` != 'root' ] && exit 1
+[ -z "$1" ] && echo "Usage: $0 <vpn-name> ..." && exit 1
+
+while [ ! -z "$1" ]; do
+    CONF=$CONFDIR/$1.conf
+    [ ! -e "$CONF" ] && echo "config file $conf does not exist" && exit 1
+    ps -A -o pid,cmd | grep "$DAEMON" | grep "$CONF" | \
+        while read pid cmdline; do
+            openfiles=$(lsof -n -p $pid)
+            if [ ! -z "$(echo $openfiles | grep $DAEMON)" ]; then
+                kill $pid && echo "$1 killed [PID $pid]"
+            fi
+        done
+
+    shift
+done
