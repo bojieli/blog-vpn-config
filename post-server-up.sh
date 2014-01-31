47a1736253b36ba72b2aaf06701cb1507611c9e5
Bojie Li <bojieli@gmail.com>
2014-01-31 22:51:03 +0800

    add a TCP server

--- post-server-up.sh
+++ post-server-up.sh
@@ -6,16 +6,20 @@ fi
 
 ip route replace 10.7.0.0/16 via 10.7.0.2 table 1002
 ip route replace 10.8.0.0/16 via 10.8.0.2 table 1002
+ip route replace 10.6.0.0/16 via 10.6.0.2 table 1002
 
 ip -6 route del 2001:da8:d800:f001::/64 dev tun0
 ip -6 route del 2001:da8:d800:f001::/64 dev tun1
+ip -6 route del 2001:da8:d800:f001::/64 dev tun2
 ip -6 route replace 2001:da8:d800:f001:160:99::/96 dev tun0
 ip -6 route replace 2001:da8:d800:f001:176:99::/96 dev tun1
+ip -6 route replace 2001:da8:d800:f001:160:98::/96 dev tun2
 
 for i in {0..9} {a..f}; do
     for j in {0..9} {a..f}; do
         ip neigh add proxy 2001:da8:d800:f001:160:99:1:$i$j dev eth0
         ip neigh add proxy 2001:da8:d800:f001:176:99:1:$i$j dev eth1
+        ip neigh add proxy 2001:da8:d800:f001:160:98:1:$i$j dev eth0
     done
 done
 
