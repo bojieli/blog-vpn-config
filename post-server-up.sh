2de3541e5a83b9e6eac8c4bd9c5c13ee7408371d
Bojie Li <bojieli@gmail.com>
2014-07-04 23:57:16 +0800

    fix openvpn ipv6 route

--- post-server-up.sh
+++ post-server-up.sh
@@ -11,19 +11,20 @@ ip route replace 10.8.0.0/16 via 10.8.0.2 table 1002
 function update_route() {
     subnet=$(ip -6 addr show dev $1 | grep inet6 | awk '{print $2}')
     new_subnet=$(echo "$subnet" | sed 's/\/64/\/96/')
-    ip -6 route del $subnet dev $1
-    ip -6 route del $new_subnet
-    ip -6 route add $new_subnet dev $1
+    ip -6 route add $new_subnet dev $1 table 1002
 }
 
+ip -6 route flush table 1002
 update_route tun0
 update_route tun1
 update_route tun2
+ip -6 rule del from all lookup 1002 pref 1000
+ip -6 rule add from all lookup 1002 pref 1000
 
 for i in {0..9} {a..f}; do
     for j in {0..9} {a..f}; do
         ip neigh add proxy 2001:da8:d800:f001:160:98:1:$i$j dev eth0
-        ip neigh add proxy 2001:da8:d800:f001:176:99:1:$i$j dev eth1
+        ip neigh add proxy 2001:da8:d800:f001:176:99:1:$i$j dev eth0
         ip neigh add proxy 2001:da8:d800:f001:160:99:1:$i$j dev eth0
     done
 done
