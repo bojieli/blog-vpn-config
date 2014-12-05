5feee8333ff4a7a6ed1de62d5fb27fa8c11a5f66
Bojie Li <bojieli@gmail.com>
2014-12-05 15:30:43 +0800

    add IPv6 TCP VPN endpoint

--- post-server-up.sh
+++ post-server-up.sh
@@ -9,6 +9,7 @@ ip route replace 10.7.0.0/16 via 10.7.0.2 table 1002
 ip route replace 10.8.0.0/16 via 10.8.0.2 table 1002
 ip route replace 10.9.0.0/16 via 10.9.0.2 table 1002
 ip route replace 10.10.0.0/16 via 10.10.0.2 table 1002
+ip route replace 10.11.0.0/16 via 10.11.0.2 table 1002
 
 function update_route() {
     subnet=$(ip -6 addr show dev $1 | grep inet6 | awk '{print $2}')
@@ -22,7 +23,8 @@ update_route tun1
 update_route tun2
 update_route tun3
 update_route tun4
-ip -6 rule del from all lookup 1002 pref 1000
+update_route tun5
+ip -6 rule del from all lookup 1002 pref 1000 2>/dev/null
 ip -6 rule add from all lookup 1002 pref 1000
 
 for i in {0..9} {a..f}; do
@@ -32,6 +34,7 @@ for i in {0..9} {a..f}; do
         ip neigh add proxy 2001:da8:d800:f001:176:99:1:$i$j dev eth0
         ip neigh add proxy 2001:da8:d800:f001:160:99:1:$i$j dev eth0
         ip neigh add proxy 2001:da8:d800:f001:160:100:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:160:101:1:$i$j dev eth0
     done
 done
 
