de1e7caf8557ab7451aa1074abe15c4fc213f983
Bojie Li <bojieli@gmail.com>
2015-03-19 17:51:36 +0800

    By Yifan Gao: add 4 workers to load balance VPN connections

--- post-server-up.sh
+++ post-server-up.sh
@@ -3,7 +3,10 @@
 if [ `whoami` != 'root' ]; then
     exit 1
 fi
-
+ip route replace 10.1.0.0/16 via 10.1.0.2 table 1002
+ip route replace 10.2.0.0/16 via 10.2.0.2 table 1002
+ip route replace 10.3.0.0/16 via 10.3.0.2 table 1002
+ip route replace 10.4.0.0/16 via 10.4.0.2 table 1002
 ip route replace 10.6.0.0/16 via 10.6.0.2 table 1002
 ip route replace 10.7.0.0/16 via 10.7.0.2 table 1002
 ip route replace 10.8.0.0/16 via 10.8.0.2 table 1002
@@ -24,6 +27,11 @@ update_route tun2
 update_route tun3
 update_route tun4
 update_route tun5
+update_route tun-worker-1
+update_route tun-worker-2
+update_route tun-worker-3
+update_route tun-worker-4
+
 ip -6 rule del from all lookup 1002 pref 1000 2>/dev/null
 # this is to ensure local subnet do not go through gateway
 ip -6 route add 2001:da8:d800:f001::/64 dev eth0 table 1002 2>/dev/null
@@ -37,6 +45,11 @@ for i in {0..9} {a..f}; do
         ip neigh add proxy 2001:da8:d800:f001:160:99:1:$i$j dev eth0
         ip neigh add proxy 2001:da8:d800:f001:160:100:1:$i$j dev eth0
         ip neigh add proxy 2001:da8:d800:f001:160:101:1:$i$j dev eth0
+
+        ip neigh add proxy 2001:da8:d800:f001:100:1:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:2:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:3:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:4:1:$i$j dev eth0
     done
 done
 
