70a42ac495f1a0e74396d342dd724ab249b2f3f1
Bojie Li <bojieli@gmail.com>
2015-03-24 15:05:38 +0800

    add worker process to 16

--- post-server-up.sh
+++ post-server-up.sh
@@ -14,6 +14,19 @@ ip route replace 10.9.0.0/16 via 10.9.0.2 table 1002
 ip route replace 10.10.0.0/16 via 10.10.0.2 table 1002
 ip route replace 10.11.0.0/16 via 10.11.0.2 table 1002
 
+ip route replace 10.21.0.0/16 via 10.21.0.2 table 1002
+ip route replace 10.22.0.0/16 via 10.22.0.2 table 1002
+ip route replace 10.23.0.0/16 via 10.23.0.2 table 1002
+ip route replace 10.24.0.0/16 via 10.24.0.2 table 1002
+ip route replace 10.25.0.0/16 via 10.25.0.2 table 1002
+ip route replace 10.26.0.0/16 via 10.26.0.2 table 1002
+ip route replace 10.27.0.0/16 via 10.27.0.2 table 1002
+ip route replace 10.28.0.0/16 via 10.28.0.2 table 1002
+ip route replace 10.29.0.0/16 via 10.29.0.2 table 1002
+ip route replace 10.30.0.0/16 via 10.30.0.2 table 1002
+ip route replace 10.31.0.0/16 via 10.31.0.2 table 1002
+ip route replace 10.32.0.0/16 via 10.32.0.2 table 1002
+
 function update_route() {
     subnet=$(ip -6 addr show dev $1 | grep inet6 | awk '{print $2}')
     new_subnet=$(echo "$subnet" | sed 's/\/64/\/96/')
@@ -31,6 +44,18 @@ update_route tun-worker-1
 update_route tun-worker-2
 update_route tun-worker-3
 update_route tun-worker-4
+update_route tun-worker-5
+update_route tun-worker-6
+update_route tun-worker-7
+update_route tun-worker-8
+update_route tun-worker-9
+update_route tun-worker-10
+update_route tun-worker-11
+update_route tun-worker-12
+update_route tun-worker-13
+update_route tun-worker-14
+update_route tun-worker-15
+update_route tun-worker-16
 
 ip -6 rule del from all lookup 1002 pref 1000 2>/dev/null
 # this is to ensure local subnet do not go through gateway
@@ -50,6 +75,18 @@ for i in {0..9} {a..f}; do
         ip neigh add proxy 2001:da8:d800:f001:100:2:1:$i$j dev eth0
         ip neigh add proxy 2001:da8:d800:f001:100:3:1:$i$j dev eth0
         ip neigh add proxy 2001:da8:d800:f001:100:4:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:21:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:22:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:23:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:24:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:25:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:26:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:27:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:28:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:29:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:30:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:31:1:$i$j dev eth0
+        ip neigh add proxy 2001:da8:d800:f001:100:32:1:$i$j dev eth0
     done
 done
 
